3077893bc0a20f1e1464e6223577a12bc62570a5
Franky Vertriest <Franky.Vertriest@tothepoint.company>
2020-03-04 14:46:55 +0100

    additional test

--- src/test/java/be/jcrafters/kata/selfdriving/SubmissionTest.java
+++ src/test/java/be/jcrafters/kata/selfdriving/SubmissionTest.java
@@ -71,6 +71,19 @@ class SubmissionTest {
         assertThat(score).isEqualTo(2);
     }
 
+    @Test
+    void getScore_ComplexRides() {
+        FulfilledRide ride1 = createRide(0, 0, 1, 0, false);
+        FulfilledRide ride2 = createRide(0, 0, 1, 0, false);
+        FulfilledRide ride3 = createRide(10, 10, 10, 200, true);
+        FulfilledRide ride4 = createRide(8, 8, 2, 2, true);
+        List<Set<FulfilledRide>> assignments = List.of(Set.of(ride1, ride2, ride3, ride4));
+
+        long score = new Submission(assignments).getScore(10);
+
+        assertThat(score).isEqualTo(1 + 1 + 200 + 22);
+    }
+
     private FulfilledRide createRide(int a, int b, int x, int y, boolean punctual) {
         return new FulfilledRide(aRide()
                 .withStart(Coordinate.of(a, b))
